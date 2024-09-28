10a37e75576db33bab17ad4bd29c619e334d24b0
tahmid-saj <tahmid.saj@gmail.com>
2024-09-28 18:05:22 -0400

    market data context

---
+++ src/contexts/market/market.context.tsx
@@ -0,0 +1,80 @@
+import { createContext, useState, FC } from "react";
+
+import { validateMarketDataQuery } from "../../utils/validations/market.validations";
+import { DEFAULT_MARKET_DATA, MARKET_DATA_TYPES } from "../../utils/constants/market.constants";
+
+import { getStockMarketData, getIndicesMarketData,
+  getCryptoMarketData, getForexMarketData
+} from "../../utils/api-requests/market.requests";
+
+import { MarketDataQuery, MarketDataQueryResult, MarketData, MarketDataProviderProps, MarketDataContextType } from "./market.types";
+
+// helper functions
+const searchMarketDataHelper = async (marketDataQuery: MarketDataQuery): Promise<MarketDataQueryResult[] | undefined> => {
+  if (!validateMarketDataQuery(marketDataQuery)) return DEFAULT_MARKET_DATA
+
+  // api request
+  let res
+  switch (marketDataQuery.marketDataType) {
+    case MARKET_DATA_TYPES.stocks:
+      res = await getStockMarketData(marketDataQuery)
+      break
+    case MARKET_DATA_TYPES.indices:
+      res = await getIndicesMarketData(marketDataQuery)
+      break
+    case MARKET_DATA_TYPES.crypto:
+      res = await getCryptoMarketData(marketDataQuery)
+      break
+    case MARKET_DATA_TYPES.currencies:
+      res = await getForexMarketData(marketDataQuery)
+      break
+    default:
+      break
+  }
+
+  return res
+}
+
+// initial state
+export const MarketDataContext = createContext<MarketDataContextType>({
+  marketData: DEFAULT_MARKET_DATA,
+  // marketData structure
+  // {
+  //   marketDataType: "stocks",
+  //   marketDataTicker: "AAPL",
+  //   marketDataInterval: "day",
+  //   marketDataStartDate: "2024-01-01",
+  //   marketDataEndDate: "2024-04-01",
+  //   queryResults: [
+  //     {
+  //       closing: 100,
+  //       time: "2024-01-01"
+  //     }
+  //   ]
+  // }
+  // 
+
+  searchMarketData: () => {}
+})
+
+// context component
+export const MarketDataProvider: FC<MarketDataProviderProps> = ({ children }) => {
+  const [marketData, setMarketData] = useState<MarketData | undefined>(DEFAULT_MARKET_DATA)
+
+  const searchMarketData = async (marketDataQuery: MarketDataQuery) => {
+    const resMarketData = await searchMarketDataHelper(marketDataQuery)
+    setMarketData({
+      ...marketDataQuery,
+      queryResults: resMarketData as MarketDataQueryResult[]
+    })
+  }
+
+  const value = { marketData, setMarketData, searchMarketData }
+
+  return (
+    <MarketDataContext.Provider
+      value={ value }>
+        { children }
+    </MarketDataContext.Provider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { MarketDataProvider } from './contexts/market/market.context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <MarketDataProvider>
+        <App />
+      </MarketDataProvider>
     </BrowserRouter>
   </StrictMode>,
 )
